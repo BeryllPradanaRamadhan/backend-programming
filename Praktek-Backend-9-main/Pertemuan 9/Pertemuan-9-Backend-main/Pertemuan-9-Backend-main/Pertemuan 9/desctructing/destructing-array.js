83e404fbfd21d4c4a003d910280347ae4a2c31ff
BeryllPradanaRamadhan <92988064+BeryllPradanaRamadhan@users.noreply.github.com>
2022-12-10 12:14:38 +0700

    Pertemuan 9

---
+++ Praktek-Backend-9-main/Pertemuan 9/Pertemuan-9-Backend-main/Pertemuan-9-Backend-main/Pertemuan 9/desctructing/destructing-array.js
@@ -0,0 +1,7 @@
+const family = ["Beryll", "Lina", "Harry", "Ad
+i"];
+
+const [husband, wife, firstChild, lastChild] = family
+
+console.log(husband);
+console.log(wife);
---
+++ Praktek-Backend-9-main/Pertemuan 9/Pertemuan-9-Backend-main/Pertemuan-9-Backend-main/Pertemuan 9/variable.js
@@ -0,0 +1,9 @@
+// membuat variable dengan keyword const.
+// variable (const) tidak dapat diubah nilainya
+
+const name = "Beryll Pradana Ramadhan";
+const age = 19;
+const major = "Teknik Informatika";
+
+
+console.log(name, age, major)
